ad9ee165c4f2a17faa53d4e3fe09a6722c54165c
MaximeD <144691184+MaximeDro@users.noreply.github.com>
2023-11-22 12:17:45 +0100

    Update my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -4,27 +4,50 @@ import streamlit as st
 
 st.title('Hello Wilders, welcome to my application!')
 
-st.write("I enjoy to discover stremalit possibilities")
+st.write("Enjoy the life!")
 
-name = st.text_input("Please give me your name :")
-name_length = len(name)
-st.write("Your name has ",name_length,"characters")
+country = st.text_input("Please give me your country:")
+country_length = len(country)
+st.write("Do you know that their is ",country_length,"characters in ", country)
 
-link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
-df_weather = pd.read_csv(link)
+link = 'https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv'
+df_car = pd.read_csv(link)
+
+
+country_choice = st.sidebar.selectbox(
+    "Which continent?",
+    (" ","Europe", "Japan", "US"))
+
+st.title('Here is the database of car sold in ')
+
+# st.radio(label, options, index=0, format_func=special_internal_function, 
+# 	key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, 
+# 	horizontal=False, captions=None, label_visibility="visible")
 
 # Here we use "magic commands":
-df_weather
-# Graphique
-st.line_chart(df_weather['MAX_TEMPERATURE_C'])
-# Figure
+df_car[df_car['continent'].str.contains(country_choice)]
+
+
+
 import seaborn as sns
-viz_correlation = sns.heatmap(df_weather.drop(columns = ['OPINION','DATE']).corr(), 
+import matplotlib.pyplot as plt
+# Figure 
+viz_correlation = sns.heatmap(df_car.drop(columns = ['continent']).corr(), 
 								center=0,
 								cmap = sns.color_palette("vlag", as_cmap=True)
 								)
 
 st.pyplot(viz_correlation.figure)
+
+# Graphique
+st.set_option('deprecation.showPyplotGlobalUse', False)
+fig, ax = plt.subplots()
+ax.scatter(x=df_car[df_car['continent'].str.contains(country_choice)]['year'],
+	 y=df_car[df_car['continent'].str.contains(country_choice)]['mpg'])
+plt.xlabel('Année')
+plt.ylabel('Miles per Gallon (MPG)')
+plt.title('MPG par année')
+st.pyplot()
 
 
 # Pour afficher le lien vers l'API (ou executer l'API)
